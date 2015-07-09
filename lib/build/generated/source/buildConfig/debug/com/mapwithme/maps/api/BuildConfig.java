a8c27c4817814bb1a48a2c95f1f7637e8913f655
brazer <salanevich.anatol@gmail.com>
2015-07-09 09:36:45 +0300

    commit changes

--- lib/build/generated/source/buildConfig/debug/com/mapwithme/maps/api/BuildConfig.java
+++ lib/build/generated/source/buildConfig/debug/com/mapwithme/maps/api/BuildConfig.java
@@ -5,7 +5,7 @@ package com.mapwithme.maps.api;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.mapwithme.maps.api";
+  public static final String APPLICATION_ID = "com.mapwithme.maps.api";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
